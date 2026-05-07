bfc2f8f82b04b47e6970d2e49ec833fd289ee153
swedishllama1 <204344850+swedishllama1@users.noreply.github.com>
2026-05-07 16:35:34 +0200

    Lade till Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
+    mode: 'production',
     plugins: [
         new CopyWebpackPlugin({
             patterns: [{
